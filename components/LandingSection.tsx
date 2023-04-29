1c3782a7cd8b73e1d80911b0ab23c7056c79dee0
Gloria <twebazegloria909@gmail.com>
2023-04-29 17:06:31 +0300

    animated the site

--- components/LandingSection.tsx
+++ components/LandingSection.tsx
@@ -2,29 +2,33 @@ import { Title } from "@/commons";
 import {
   AiBody,
   AiSub,
-  AiTitle,
   ButtonContainer,
   LandingContainer,
   LandingWrapper,
   LearnButton,
   ProjectButton,
 } from "./Landing.Styles";
+import { Fade } from "react-awesome-reveal";
 
 export default function LandingSection() {
   return (
     <LandingContainer>
       <LandingWrapper>
-        <Title>Unlocking The Future</Title>
-        <AiSub>NGENI AI</AiSub>
-        <AiBody>
-          Empower your business with the future of technology. Join us in
-          revolutionizing industries with our cutting-edge AI and machine
-          learning solutions.
-        </AiBody>
-        <ButtonContainer>
-          <LearnButton>Learn More</LearnButton>
-          <ProjectButton>Projects</ProjectButton>
-        </ButtonContainer>
+        <Fade  duration={2000} direction="down">
+          <Title>Unlocking The Future</Title>
+          <AiSub>NGENI AI</AiSub>
+        </Fade>
+        <Fade  direction="up" duration={2000}>
+          <AiBody>
+            Empower your business with the future of technology. Join us in
+            revolutionizing industries with our cutting-edge AI and machine
+            learning solutions.
+          </AiBody>
+          <ButtonContainer>
+            <LearnButton>Learn More</LearnButton>
+            <ProjectButton>Projects</ProjectButton>
+          </ButtonContainer>
+        </Fade>
       </LandingWrapper>
     </LandingContainer>
   );
